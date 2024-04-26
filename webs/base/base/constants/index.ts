190007998dfbb242c34fd43a1c626a7d2dc271d6
MateoVargasInvow <mateo.vargas@invow.com>
2024-04-26 08:52:48 -0300

    comienzo

---
+++ webs/base/base/constants/index.ts
@@ -0,0 +1,40 @@
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: "Learn More",
+    links: [
+      { label: "Terms and Conditions", href: "/terms-and-conditions" },
+      { label: "Privacy Policy", href: "/privacy-policy" },
+    ],
+  },
+  {
+    title: "Navigation",
+    links: [
+      { label: "Services", href: "/#services" },
+      { label: "Portfolio", href: "/#portfolio" },
+      { label: "Contact", href: "/contact-us" },
+      { label: "About us", href: "/about-us" },
+      // { label: "Blog", href: "blog" },
+    ],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: "Contact Us",
+  links: [
+    { label: "Email", value: "contact@invow.com" },
+    { label: "Location", value: "AV. Avellaneda 462 1C \n Argentina, Tandil" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Social",
+  data: [
+    {
+      image: "/facebook.svg",
+      href: "https://www.facebook.com/profile.php?id=61556379895964/",
+    },
+    { image: "/instagram.svg", href: "https://www.instagram.com/invow.tech/" },
+    { image: "/x.svg", href: "https://twitter.com/InvowClub/" },
+  ],
+};
