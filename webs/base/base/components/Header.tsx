a711a4974b2ef8dadae09b1ab12e66edb5f14781
agustin Lezica <agustinflezica@gmail.com>
2024-04-26 10:00:54 -0300

    inicio

---
+++ webs/base/base/components/Header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const Header = () => {
+    return(
+        
+        <h1 className="mb-3 text-2xl font-semibold text-center">
+           Web ecommerce
+        </h1>
+    )
+}
+export default Header;
