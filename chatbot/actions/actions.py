cafcdd3d9c29334a45678300d62a8212a8981211
SantiagoMRodriguez <santiagoorodriguez1@gmail.com>
2024-05-04 21:12:02 -0300

    primer commit de santiago

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,15 +1,15 @@
-from typing import Any, Text, Dict, List, Tuple
+import threading
+from generator.Generator import Generator
+from generator.Front import Front
+import CONSTANTS
+from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-import threading
+from rasa_sdk.events import SlotSet
+import datetime
+import os.path
+import json
 
-from generator.Generator import Generator
-from generator.DBManager import DBManager
-from generator.PageRunner import PageRunner
-from generator.Front import Front
-from generator.Back import Back
-import generator.GenericRoutes
-import CONSTANTS
 
 class ActionCrearPagina(Action):
 
@@ -20,7 +20,7 @@ class ActionCrearPagina(Action):
         creating_thread = threading.Thread(Generator.create_project(user=tracker.sender_id, page_name=tracker.get_slot('page_name')))
         creating_thread.start()
         creating_thread.join()
-        DBManager.add_user_page(user=tracker.sender_id, page_name=tracker.get_slot('page_name'))
+        #DBManager.add_user_page(user=tracker.sender_id, page_name=tracker.get_slot('page_name'))
         print("------------PAGINA CREADA---------")
         message = "Tu pagina fue creada con exito."
         dispatcher.utter_message(message)
@@ -92,7 +92,146 @@ class ActionEjecutarPagina(Action):
 
             # Matar la página
             Generator.running[(user, page_name)][1].stop()
+
             Generator.running.pop((user, page_name))
 
             # Decrementar puertos
             Generator.dec_port(Generator.current_front_port, CONSTANTS.MIN_FRONT_PORT)
+
+
+# Saludo Actions
+class ActionSaludoTelegram(Action):
+
+    def name(self) -> Text:
+        return "action_saludo_telegram"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        now = datetime.datetime.now()
+        hora = int(now.strftime("%H"))
+        if (hora > 0) and (hora <= 12):
+            horario = "mañana"
+        elif (hora > 12) and (hora <= 18):
+            horario = "tarde"
+        else:
+            horario = "noche"
+        variable = tracker.latest_message["metadata"]["message"]
+        nombre = variable["from"]["first_name"]
+        SlotSet("usuario", nombre)
+        SlotSet("horario", horario)
+        message = "Hola " + nombre + ", como va tu " + horario + "? Soy el Chatbot WebGenerator, el encargado de ayudarte a crear tu pagina web! Si queres preguntame y te explico un poco en que cosas puedo contribuir."
+        dispatcher.utter_message(text=str(message))
+        return []
+
+
+class ActionDespedidaTelegram(Action):
+
+    def name(self) -> Text:
+        return "action_despedida_telegram"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        now = datetime.datetime.now()
+        hora = now.strftime("%H")
+        if (hora > "0") and (hora <= "12"):
+            horario = "mañana"
+        elif (hora > "12") and (hora <= "19"):
+            horario = "tarde"
+        else:
+            horario = "noche"
+        variable = tracker.latest_message["metadata"]["message"]
+        nombre = variable["from"]["first_name"]
+        message = "Que siga bien su " + horario + ", nos vemos " + nombre + "!!"
+        dispatcher.utter_message(text=str(message))
+        return []
+
+
+# Action Devolver Hora
+class ActionHora(Action):
+
+    def name(self) -> Text:
+        return "action_hora"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        now = datetime.datetime.now()
+        dispatcher.utter_message("Hoy es " + now.strftime("%d/%m de 20%y"))
+        hora = now.strftime("%H")
+        if (hora > "2") and (hora <= "13"):
+            dispatcher.utter_message("Y la hora es " + now.strftime("%H:%M") + " , tempranito ajaj")
+        elif (hora > "13") and (hora <= "19"):
+            dispatcher.utter_message("Y la hora es " + now.strftime("%H:%M") + " , hora de la siesta jajaj")
+        else:
+            dispatcher.utter_message("Y la hora es " + now.strftime("%H:%M") + " , medio tarde jaja")
+        return []
+
+
+# Estado de animo Action
+class ActionAnimo(Action):
+
+    def name(self) -> Text:
+        return "action_animo"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        intent = tracker.latest_message['intent'].get('name')
+        if (intent == "estado_de_animo_feliz"):
+            SlotSet("animo", "Feliz")
+            dispatcher.utter_message(template="utter_estado_de_animo_feliz")
+        else:
+            SlotSet("animo", "Triste")
+            dispatcher.utter_message(template="utter_estado_de_animo_triste")
+        return []
+
+
+class ActionTriste(Action):
+
+    def name(self) -> Text:
+        return "action_triste"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        triste = tracker.latest_message['intent'].get('name')
+        if str(triste) == 'triste_facu':
+            dispatcher.utter_message(template="utter_triste_facu")
+        else:
+            dispatcher.utter_message(template="utter_triste_pelea")
+        return []
+
+# Json actions
+class OperarArchivoDia():
+
+    @staticmethod
+    def guardar(AGuardar):
+        with open(".\\actions\\dia", "w") as archivo_descarga:
+            json.dump(AGuardar, archivo_descarga, indent=4)
+        archivo_descarga.close()
+
+    @staticmethod
+    def cargarArchivo():
+        if os.path.isfile(".\\actions\\dia"):
+            with open(".\\actions\\dia", "r") as archivo_carga:
+                retorno = json.load(archivo_carga)
+                archivo_carga.close()
+        else:
+            retorno = {}
+        return retorno
+
+
+class OperarArchivoUser():
+
+    @staticmethod
+    def guardar(AGuardar):
+        with open(".\\actions\\user", "w") as archivo_descarga:
+            json.dump(AGuardar, archivo_descarga, indent=4)
+        archivo_descarga.close()
+
+    @staticmethod
+    def cargarArchivo():
+        if os.path.isfile(".\\actions\\user"):
+            with open(".\\actions\\user", "r") as archivo_carga:
+                retorno = json.load(archivo_carga)
+                archivo_carga.close()
+        else:
+            retorno = {}
+        return retorno
