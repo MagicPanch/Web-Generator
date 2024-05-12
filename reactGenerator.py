2a289c92444808a9cdca2b6e082de5b36f070453
agustin Lezica <agustinflezica@gmail.com>
2024-05-12 19:52:14 -0300

    anda

--- reactGenerator.py
+++ reactGenerator.py
@@ -33,52 +33,51 @@ def generarHeader(data):
 
 def generarFooter(dataDicFooter):
     print("genero footer")
-    text = f""""
-
-    export const FOOTER_LINKS = [
-  {{
-    title: "Navegación",
-    links: [
-      {{ label: "Services", href: "/#services" }},
-      {{ label: "Portfolio", href: "/#portfolio" }},
-      {{ label: "Contact", href: "/contact-us" }},
-      {{ label: "About us", href: "/about-us" }},
-    ],
-  }},
-];
-
-export const FOOTER_CONTACT_INFO = {{
-  title: "Contactanos",
-  links: [
-    {{ label: "Email", value: {dataDicFooter["email"]} }},
-    {{ label: "Ubicación", value: {dataDicFooter["ubicacion"]} }},
-  ],
-}};
-
-export const SOCIALS = {{
-  title: "Social",
-  data: [
+    dataDicFooter = json.loads(dataDicFooter)
+    text = f""" export const FOOTER_LINKS = [
     {{
-      image: "/facebook.svg",
-      href: "https://www.facebook.com",
+        title: "Navegacion",
+        links: [
+        {{ label: "Services", href: "/#services" }},
+        {{ label: "Portfolio", href: "/#portfolio" }},
+        {{ label: "Contact", href: "/contact-us" }},
+        {{ label: "About us", href: "/about-us" }},
+        ],
     }},
-    {{ image: "/instagram.svg", href: "https://www.instagram.com" }},
-    {{ image: "/x.svg", href: "https://twitter.com" }},
-  ],
-}};
+    ];
+
+    export const FOOTER_CONTACT_INFO = {{
+    title: "Contactanos",
+    links: [
+        {{ label: "Email", value: "{dataDicFooter["email"]}" }},
+        {{ label: "Ubicacion", value: "{dataDicFooter["ubicacion"]}" }},
+    ],
+    }};
+
+    export const SOCIALS = {{
+    title: "Social",
+    data: [
+        {{
+        image: "/facebook.svg",
+        href: "https://www.facebook.com"
+        }},
+        {{ image: "/instagram.svg", href: "https://www.instagram.com" }},
+        {{ image: "/x.svg", href: "https://twitter.com" }},
+    ],
+    }};
     """
 
-    with open(dataDicFooter["address"]+"\constants\footer.ts", "w") as file:
+    with open(dataDicFooter["address"]+"\constants\/footer.ts", "w") as file:
       file.write(text)
 
 
-addres ="C:/Users/Agustin/Desktop/DesingLabelBranchSanti/Web-Generator/webs/base/base" #direccion donde se ubica la web react
+addres ="C:/Users/Agustin/Desktop/DesingLabel22/Web-Generator/webs/base/base" #direccion donde se ubica la web react
 dataHeader =  { "titulo": "ecommerce" , "address":addres , "addressLogo": "./logo.png" ,"colorTitulo":"text-yellow-600"} 
 dataHeader = json.dumps(dataHeader)
 print(dataHeader)
 dataDicHeader = json.loads(dataHeader)
 print(dataDicHeader["titulo"])
 
-dataFooter =  {"address":addres , "email":"contactDesignLabel@gmail.com", "ubicacion": "Pinto 400 Argentina, Tandil"} 
+dataFooter =  {"address":addres , "email":"contactDesignLabel@gmail.com", "ubicacion": "Pinto 401 Argentina, Tandil"} 
 dataFooter = json.dumps(dataFooter)
 generarFooter(dataFooter)
