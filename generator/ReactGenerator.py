7f9bf5e21bba66f8b87b6776729528147cd68110
Lucas Pérez <lucasperez2001@gmail.com>
2024-05-13 19:06:05 -0300

    full merge front

--- generator/ReactGenerator.py
+++ generator/ReactGenerator.py
@@ -1,3 +1,6 @@
+import json
+
+
 class ReactGenerator:
 
     _instance = None
@@ -16,16 +19,17 @@ class ReactGenerator:
 
     @staticmethod
     def generarHeader(data):
+        print("genero header")
         text = f""""use client";
-
+    
         import React from "react";
-        import logo from './logo.png';
+        import logo from '{data["addressLogo"]}';
         import Image from "next/image";
         import Link from "next/link";
-
+        
         const Header = () =>  {{
             return(
-
+            
                 <div className=" bg-blue-500 flex  items-center justify-between h-20 px-4">
                     <div className="border-green-300 border-2  rounded h-30 w-30  bg-green-400 items-center">
                         <Image src= {{logo}}  
@@ -41,6 +45,76 @@ class ReactGenerator:
         }}
         export default Header;
         """
-        with open("Header.tsx", "w") as file:
+        with open(data["address"]+"\components\Header.tsx", "w") as file:
+            file.write(text)
+            file.close()
+
+    @staticmethod
+    def generarFooter(dataFooter):
+        print("genero footer")
+        dataFooter = json.loads(dataFooter)
+        text = f""" export const FOOTER_LINKS = [
+        {{
+            title: "Navegacion",
+            links: [
+            {{ label: "Services", href: "/#services" }},
+            {{ label: "Portfolio", href: "/#portfolio" }},
+            {{ label: "Contact", href: "/contact-us" }},
+            {{ label: "About us", href: "/about-us" }},
+            ],
+        }},
+        ];
+    
+        export const FOOTER_CONTACT_INFO = {{
+        title: "Contactanos",
+        links: [
+            {{ label: "Email", value: "{dataFooter["email"]}" }},
+            {{ label: "Ubicacion", value: "{dataFooter["ubicacion"]}" }},
+        ],
+        }};
+    
+        export const SOCIALS = {{
+        title: "Social",
+        data: [
+            {{
+            image: "/facebook.svg",
+            href: "https://www.facebook.com"
+            }},
+            {{ image: "/instagram.svg", href: "https://www.instagram.com" }},
+            {{ image: "/x.svg", href: "https://twitter.com" }},
+        ],
+        }};
+        """
+
+        with open(dataFooter["address"]+"\constants//footer.ts", "w") as file:
+            file.write(text)
+            file.close()
+
+    @staticmethod
+    def generarBody(dataBody):
+        print("genero body")
+        dataBody = json.loads(dataBody)
+        text = f""" export const CARDS_DATA = [
+        {{
+            image: "https://via.placeholder.com/300",
+            title: "Producto 1",
+            description: "Descripción breve del producton 1",
+            price: "340",
+        }},
+        {{
+            image: "https://via.placeholder.com/300",
+            title: "Producto 2",
+            description: "Descripción breve del producto 2",
+            price: "5000",
+        }},
+        {{
+            image: "https://via.placeholder.com/300",
+            title: "Producto 3",
+            description: "Descripción breve del producto 3",
+            price: "{dataBody["price"]}",
+        }},
+        ];  """
+
+        with open(dataBody["address"] + "\constants\/body.ts", "w") as file:
             file.write(text)
             file.close()
