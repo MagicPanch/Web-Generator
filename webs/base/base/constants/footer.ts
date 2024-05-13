0d46f504b8dcc1d0002f4754edfa9e7c843e6001
vlDevelopment <mateo.vargas@invow.com>
2024-05-12 23:00:50 -0300

    Update footer.ts

--- webs/base/base/constants/footer.ts
+++ webs/base/base/constants/footer.ts
@@ -1,32 +1,31 @@
- export const FOOTER_LINKS = [
-    {
-        title: "Navegacion",
-        links: [
-        { label: "Services", href: "/#services" },
-        { label: "Portfolio", href: "/#portfolio" },
-        { label: "Contact", href: "/contact-us" },
-        { label: "About us", href: "/about-us" },
-        ],
-    },
-    ];
-
-    export const FOOTER_CONTACT_INFO = {
-    title: "Contactanos",
+export const FOOTER_LINKS = [
+  {
+    title: "Navegacion",
     links: [
-        { label: "Email", value: "contactDesignLabel@gmail.com" },
-        { label: "Ubicacion", value: "Pinto 401 Argentina, Tandil" },
+      { label: "Services", href: "/#services" },
+      { label: "Portfolio", href: "/#portfolio" },
+      { label: "Contact", href: "/contact-us" },
+      { label: "About us", href: "/about-us" },
     ],
-    };
+  },
+];
 
-    export const SOCIALS = {
-    title: "Social",
-    data: [
-        {
-        image: "/facebook.svg",
-        href: "https://www.facebook.com"
-        },
-        { image: "/instagram.svg", href: "https://www.instagram.com" },
-        { image: "/x.svg", href: "https://twitter.com" },
-    ],
-    };
-    
+export const FOOTER_CONTACT_INFO = {
+  title: "Contactanos",
+  links: [
+    { label: "Email", value: "contactDesignLabel@gmail.com" },
+    { label: "Ubicacion", value: "Pinto 401 Argentina, Tandil" },
+  ],
+};
+
+export const SOCIALS = {
+  title: "Social",
+  data: [
+    {
+      image: "/facebook.svg",
+      href: "https://www.facebook.com",
+    },
+    { image: "/instagram.svg", href: "https://www.instagram.com" },
+    { image: "/x.svg", href: "https://twitter.com" },
+  ],
+};
