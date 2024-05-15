26228da36aee1ae78c60e48eaacbedd41de8e2be
agustin Lezica <agustinflezica@gmail.com>
2024-05-15 10:13:04 -0300

    se puede pasar color de titulo de header como hexadecimal

--- webs/base/base/constants/footer.ts
+++ webs/base/base/constants/footer.ts
@@ -1,30 +1,32 @@
-export const FOOTER_LINKS = [
-  {
-    title: "Navegacion",
-    links: [
-      { label: "Inicio", href: "/" },
-      { label: "Shop", href: "/shop" },
-      { label: "Contacto", href: "/contact" },
-    ],
-  },
-];
-
-export const FOOTER_CONTACT_INFO = {
-  title: "Contactanos",
-  links: [
-    { label: "Email", value: "contactDesignLabel@gmail.com" },
-    { label: "Ubicacion", value: "Pinto 401 Argentina, Tandil" },
-  ],
-};
-
-export const SOCIALS = {
-  title: "Social",
-  data: [
+ export const FOOTER_LINKS = [
     {
-      image: "/facebook.svg",
-      href: "https://www.facebook.com",
+        title: "Navegacion",
+        links: [
+        { label: "Services", href: "/#services" },
+        { label: "Portfolio", href: "/#portfolio" },
+        { label: "Contact", href: "/contact-us" },
+        { label: "About us", href: "/about-us" },
+        ],
     },
-    { image: "/instagram.svg", href: "https://www.instagram.com" },
-    { image: "/x.svg", href: "https://twitter.com" },
-  ],
-};
+    ];
+
+    export const FOOTER_CONTACT_INFO = {
+    title: "Contactanos",
+    links: [
+        { label: "Email", value: "contactDesignLabel@gmail.com" },
+        { label: "Ubicacion", value: "Pinto 401 Argentina, Tandil" },
+    ],
+    };
+
+    export const SOCIALS = {
+    title: "Social",
+    data: [
+        {
+        image: "/facebook.svg",
+        href: "https://www.facebook.com"
+        },
+        { image: "/instagram.svg", href: "https://www.instagram.com" },
+        { image: "/x.svg", href: "https://twitter.com" },
+    ],
+    };
+    
--- webs/base/base/tailwind.config.ts
+++ webs/base/base/tailwind.config.ts
@@ -1,39 +1,40 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
+    module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
     extend: {
-      fontFamily: {
-        body: ["Korolev Medium"],
-      },
-      colors: {
-        primary: {
-          400: "#CBEAF2",
-          500: "#66b7cb",
-          600: "#55a4b7",
+        fontFamily: {
+            body: ["Korolev Medium"],
         },
-        bgGray: "#E2E2E2",
-        bgBlack: "#1C1C1C",
-      },
-      backgroundSize: {
-        "16": "4rem",
-      },
-      screens: {
-        xs: "400px",
-        "3xl": "1680px",
-        "4xl": "2200px",
-      },
-      maxWidth: {
-        "10xl": "1512px",
-      },
-      borderRadius: {
-        "5xl": "40px",
-      },
-    },
-  },
-  plugins: [],
-};
+    colors: {
+        primary: {
+            400: "#CBEAF2",
+            500: "#66b7cb",
+            600: "#55a4b7",
+        },
+            bgGray: "#E2E2E2",
+            bgBlack: "#1C1C1C",
+            colorTituloHeader : "#E1704F",
+        },
+        backgroundSize: {
+            "16": "4rem",
+        },
+        screens: {
+            xs: "400px",
+            "3xl": "1680px",
+             "4xl": "2200px",
+        },
+        maxWidth: {
+            "10xl": "1512px",
+        },
+        borderRadius: {
+            "5xl": "40px",
+          },
+         },
+        },
+        plugins: [],
+    };
